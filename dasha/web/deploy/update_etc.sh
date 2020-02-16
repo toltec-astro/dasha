c996ed0a0d5d98b37039670a94fdb77740f77d6b
Zhiyuan Ma <zhiyuanma@umass.edu>
2020-02-16 16:33:42 -0500

    add example deploy files

---
+++ dasha/web/deploy/update_etc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for p in /etc/apache2/sites-enabled/500-gunicorn.conf \
+	/etc/systemd/system/gunicorn@.socket \
+	/etc/systemd/system/gunicorn@.service \
+	/etc/systemd/system/celery@.service \
+	/etc/systemd/system/celerybeat@.service \
+	/etc/systemd/system/celeryflower@.service; do
+	echo copy ${p} to ./
+	cp ${p} ./
+done
