ad8d7e7c19ea5ce503600c668963a7d25ed118ee
Jerry Ma <jerry.ma.nk@gmail.com>
2020-02-28 11:20:34 -0500

    add cli

--- dasha/cli.py
+++ dasha/cli.py
@@ -24,7 +24,77 @@ def load_env_helper():
                 k, v = map(str.strip, ln.split('=', 1))
                 envs[k] = v
     cmd = ' '.join(f'{k}="{v}"' for k, v in envs.items())
+    # Print the env vars so that it can be captured by the shell
     print(cmd)
+
+
+def _add_ext_arg(parser):
+    _all_ext_procs = ['flask', 'celery', 'beat', 'flower']
+    parser.add_argument(
+            'extension',
+            metavar='EXT',
+            choices=_all_ext_procs,
+            nargs='?',
+            default='flask',
+            help="The extension process to run"
+                 " Available options: {}".format(", ".join(_all_ext_procs))
+            )
+
+    def handle_ext_args(args):
+        if args.extension == 'flask':
+            from .web import create_app
+            app = create_app()
+            app.run(debug=True, port=8050)
+        elif args.extension in ['celery', 'beat', 'flower']:
+            e = args.extension
+            dispatch_cmd = {
+                    'celery': 'worker',
+                    }
+            _run_celery_cmd(dispatch_cmd.get(e, e))
+        else:
+            raise NotImplementedError
+    return parser, handle_ext_args
+
+
+def _run_celery_cmd(name):
+    import celery.bin.worker as celery_worker
+    import celery.bin.beat as celery_beat
+    from .web.celery_app import celery
+    from flower.command import FlowerCommand
+
+    class _flower(FlowerCommand):
+        def run(self, *args, **kwargs):
+            return self.run_from_argv('flower', (), **kwargs)
+
+    dispatch_cmd = {
+            'worker': celery_worker.worker,
+            'beat': celery_beat.beat,
+            'flower': _flower
+            }
+    dispatch_options = {
+            'worker': {
+                'loglevel': 'DEBUG',
+                'traceback': True,
+                },
+            }
+    cmd = dispatch_cmd[name](app=celery)
+    cmd(**dispatch_options.get('worker', dict()))
+
+
+def run():
+    """A helper utility to run dasha site."""
+
+    parser = argparse.ArgumentParser(
+            description='Run DashA site.')
+    parser.add_argument(
+            'site',
+            metavar='NAME',
+            help="The module name or path to the site. "
+                 "Examples: ~/mysite.py, mypackage.mysite")
+    parser, handle_ext_args = _add_ext_arg(parser)
+    args = parser.parse_args()
+    os.environ['DASHA_SITE'] = args.site
+    handle_ext_args(args)
 
 
 def run_demo():
@@ -40,7 +110,6 @@ def run_demo():
                     )
                 }
     all_demo_names = list(all_demos.keys())
-    all_ext_procs = ['flask', 'celery', 'beat', 'flower']
     parser = argparse.ArgumentParser(
             description='Run the DashA demos.')
     parser.add_argument(
@@ -50,44 +119,8 @@ def run_demo():
             help="The name of the demo to run."
                  " Available demos: {}".format(", ".join(all_demo_names))
             )
-    parser.add_argument(
-            'extension',
-            metavar='EXT',
-            choices=all_ext_procs,
-            nargs='?',
-            default='flask',
-            help="The extension process to run"
-                 " Available options: {}".format(", ".join(all_ext_procs))
-            )
-
+    parser, handle_ext_args = _add_ext_arg(parser)
     args = parser.parse_args()
-
     site = all_demos[args.name].as_posix()
     os.environ['DASHA_SITE'] = site
-
-    import celery.bin as celery_bin
-
-    def run_celery_service(name):
-        from .web.celery_app import celery
-        dispatch_cls = {
-                'worker': celery_bin.worker.worker,
-                }
-        dispatch_options = {
-                'worker': {
-                    'loglevel': 'DEBUG',
-                    'traceback': True,
-                    },
-                }
-        service = dispatch_cls['worker'](app=celery)
-        service.run(**dispatch_options.get('worker', dict()))
-
-    if args.extension == 'flask':
-        from .web import create_app
-        app = create_app()
-        app.run(debug=True, port=8050)
-    elif args.extension == 'celery':
-        run_celery_service('worker')
-    elif args.extension == 'flower':
-        run_celery_service('flower')
-    else:
-        raise NotImplementedError
+    handle_ext_args(args)
