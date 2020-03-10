f1eacb0350cfb32e6197d5684eb943ae25c939a7
Zhiyuan Ma <zhiyuanma@umass.edu>
2020-03-10 13:39:12 +0000

    update config

--- dasha/web/deploy/backup_etc.sh
+++ dasha/web/deploy/backup_etc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for p in /etc/apache2/sites-enabled/500-gunicorn.conf \
+	/etc/redis/redis.conf \
 	/etc/systemd/system/gunicorn@.socket \
 	/etc/systemd/system/gunicorn@.service \
 	/etc/systemd/system/celery@.service \
