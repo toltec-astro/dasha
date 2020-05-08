7ced1485a2ba8746b61a668275b3838a1f36cdcb
Jerry Ma <jerry.ma.nk@gmail.com>
2020-05-08 12:36:08 -0500

    Deployed 912498e with MkDocs version: 1.1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,10 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev22+g6c4ee58.d20200302',
+    VERSION: '0.1.dev32+g6ea9fce.d20200507',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
